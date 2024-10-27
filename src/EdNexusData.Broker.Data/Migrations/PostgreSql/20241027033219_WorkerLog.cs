08ac3c1003a5a3eb80979234c310be94d6c0e429
Makoa Jacobsen <makoa@makoajacobsen.com>
2024-10-27 04:29:09 +0000

    Add WorkerLog

---
+++ src/EdNexusData.Broker.Data/Migrations/PostgreSql/20241027033219_WorkerLog.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EdNexusData.Broker.Data.Migrations.PostgreSql
+{
+    /// <inheritdoc />
+    public partial class WorkerLog : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "WorkerLog",
+                table: "Worker_Jobs",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "WorkerLog",
+                table: "Worker_Jobs");
+        }
+    }
+}
