9780309ef0d5420dfa0a75b6ad07b2ad4d035445
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-01-19 04:24:11 +0000

    Add urls for workers

--- src/EdNexusData.Broker.Worker/WorkerEnvironment.cs
+++ src/EdNexusData.Broker.Worker/WorkerEnvironment.cs
@@ -5,5 +5,15 @@ public class WorkerEnvironment : Core.Environment
     public WorkerEnvironment(IConfiguration configuration)
     {
         EnvironmentName = configuration.GetValue<string>("Environment") ?? throw new ArgumentNullException("Missing environment");
+
+        if (IsNonProductionEnvironment())
+        {
+            var urls = configuration.GetValue<string>("EnvironmentUrls") ?? throw new ArgumentNullException("Missing environment urls");
+            var splitUrls = urls.Split(",");
+            foreach(var url in splitUrls)
+            {
+                AddAddress(url);
+            }
+        }
     }
 }
