3f714d75b8146ce2fc3d0bdb633821c09109c186
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-03-01 21:26:54 +0000

    Lower strings to make sure

--- src/EdNexusData.Broker.Core/Environment.cs
+++ src/EdNexusData.Broker.Core/Environment.cs
@@ -7,17 +7,17 @@ public abstract class Environment
     public string EnvironmentName = default!;
     public List<Uri> Addresses = new List<Uri>();
 
-    public static ImmutableList<string> NonProductionEnvironments => new List<string> { "Demo", "Development", "Test" }.ToImmutableList();
-    public static ImmutableList<string> ProductionEnvironments => new List<string> { "Production", "Live" }.ToImmutableList();
+    public static ImmutableList<string> NonProductionEnvironments => new List<string> { "demo", "development", "test" }.ToImmutableList();
+    public static ImmutableList<string> ProductionEnvironments => new List<string> { "production", "live" }.ToImmutableList();
 
     public bool IsNonProductionEnvironment()
     {
-        return !ProductionEnvironments.Contains(EnvironmentName);
+        return !ProductionEnvironments.Contains(EnvironmentName.ToLower());
     }
 
     public bool IsProductionEnvironment()
     {
-        return ProductionEnvironments.Contains(EnvironmentName);
+        return ProductionEnvironments.Contains(EnvironmentName.ToLower());
     }
 
     public void AddAddress(string uri)
