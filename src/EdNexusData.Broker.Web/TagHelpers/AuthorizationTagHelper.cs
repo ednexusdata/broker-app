16644f000c6ff07d362aee4a7fc0a9941e5aae14
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-05-11 18:05:25 +0000

    Fix null

--- src/EdNexusData.Broker.Web/TagHelpers/AuthorizationTagHelper.cs
+++ src/EdNexusData.Broker.Web/TagHelpers/AuthorizationTagHelper.cs
@@ -15,7 +15,11 @@ public class AuthorizeTagHelper : TagHelper, IAuthorizeData
     private readonly IPolicyEvaluator _policyEvaluator;
     private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public AuthorizeTagHelper(IHttpContextAccessor httpContextAccessor, IAuthorizationPolicyProvider policyProvider, IPolicyEvaluator policyEvaluator)
+    public AuthorizeTagHelper(
+        IHttpContextAccessor httpContextAccessor, 
+        IAuthorizationPolicyProvider policyProvider, 
+        IPolicyEvaluator policyEvaluator
+    )
     {
         _httpContextAccessor = httpContextAccessor;
         _policyProvider = policyProvider;
@@ -26,27 +30,32 @@ public class AuthorizeTagHelper : TagHelper, IAuthorizeData
     /// Gets or sets the policy name that determines access to the HTML block.
     /// </summary>
     [HtmlAttributeName("asp-policy")]
-    public string Policy { get; set; }
+    public string? Policy { get; set; }
 
     /// <summary>
     /// Gets or sets a comma delimited list of roles that are allowed to access the HTML  block.
     /// </summary>
     [HtmlAttributeName("asp-roles")]
-    public string Roles { get; set; }
+    public string? Roles { get; set; }
 
     /// <summary>
     /// Gets or sets a comma delimited list of schemes from which user information is constructed.
     /// </summary>
     [HtmlAttributeName("asp-authentication-schemes")]
-    public string AuthenticationSchemes { get; set; }
+    public string? AuthenticationSchemes { get; set; }
 
     public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
     {
         var policy = await AuthorizationPolicy.CombineAsync(_policyProvider, new[] { this });
 
-        var authenticateResult = await _policyEvaluator.AuthenticateAsync(policy, _httpContextAccessor.HttpContext);
+        if (policy == null)
+        {
+            return;
+        }
 
-        var authorizeResult = await _policyEvaluator.AuthorizeAsync(policy, authenticateResult, _httpContextAccessor.HttpContext, null);
+        var authenticateResult = await _policyEvaluator.AuthenticateAsync(policy, _httpContextAccessor.HttpContext!);
+
+        var authorizeResult = await _policyEvaluator.AuthorizeAsync(policy, authenticateResult, _httpContextAccessor.HttpContext!, null);
 
         if (!authorizeResult.Succeeded)
         {
