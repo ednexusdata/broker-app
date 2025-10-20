3209e6a17dc1aa923fb23e6c1e0045d9096fb85c
Makoa Jacobsen <makoa@makoajacobsen.com>
2025-10-20 02:36:20 +0000

    Show error message

--- src/EdNexusData.Broker.Web/Models/ErrorViewModel.cs
+++ src/EdNexusData.Broker.Web/Models/ErrorViewModel.cs
@@ -6,6 +6,6 @@ namespace EdNexusData.Broker.Web.Models;
 public class ErrorViewModel
 {
     public string? RequestId { get; set; }
-
+    public Exception? Exception { get; set; }
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
